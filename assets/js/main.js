f3fdaa6754f882f3ff134f2974833b863ddc06df
Naheed <89731453+summandee@users.noreply.github.com>
2021-11-28 07:38:56 -0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+   $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})  
